3277c13d5514f55ea2b1e2748a0e011ea728b032
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-23 20:42:03 +0300

    Register tasarımı yapıldı

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { StatusBar } from "react-native";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import Register from "./screens/Register";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <Login />
+      <Register />
     </>
   );
 }
---
+++ screens/Register.js
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
+
+const Register = () => {
+  const [email, setEmail] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [confirmPassword, setConfirmPassword] = React.useState();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>Yol Arkadaşım</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email..."
+          placeholderTextColor="white"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password..."
+          placeholderTextColor="white"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Confirm Password..."
+          placeholderTextColor="white"
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>SIGNUP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ff6600",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "white",
+    marginBottom: 40,
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#ffc715",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#8ee000",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "white",
+  },
+});
