38355e5a35b056965fc0fce809cb01d6d5de0cfb
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-23 20:34:54 +0300

    Login Tasarımı yapıldı

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import Home from "./src/Home";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <Home />
+      <Login />
     </>
   );
 }
