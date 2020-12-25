7839c129a4fa390307eff46dabf7d7c79e2716f8
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-25 21:43:37 +0300

    Kullanıcının uygulamaya giriş yaptıysa uygulamaya direk girebilmesi sağlandı

--- firebase/config.js
+++ firebase/config.js
@@ -1,7 +1,6 @@
 import * as firebase from "firebase";
 import "@firebase/auth";
 import "@firebase/firestore";
-
 var firebaseConfig = {
   apiKey: "AIzaSyB4PUloc_nDZYq-fTLZZQHdBoADBpJxa00",
   authDomain: "yolarkadasimapp-12194.firebaseapp.com",
@@ -15,5 +14,4 @@ var firebaseConfig = {
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
 export { firebase };
--- navigations/AuthNavigation.js
+++ navigations/AuthNavigation.js
@@ -3,23 +3,59 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../screens/Login";
 import Signup from "../screens/Register";
+import { firebase } from "../firebase/config";
+import Home from "../screens/Home";
 
 const Stack = createStackNavigator();
 
 const AuthNavigation = () => {
+  const [loading, setLoading] = React.useState(true);
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    const usersRef = firebase.firestore().collection("users");
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data();
+            setLoading(false);
+            setUser(userData);
+          })
+          .catch((error) => {
+            setLoading(false);
+          });
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return <></>;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{ headerShown: false }}
-        />
+        {user ? (
+          <Stack.Screen name="Home" component={Home} />
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Signup"
+              component={Signup}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
