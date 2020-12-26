ea2e3dbcffbc093c3360407695240201d614efda
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-26 21:57:19 +0300

    TabNavigation oluşturuldu

---
+++ navigations/TabNavigation.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import HomeIcon from "../components/Home";
+import ProfileIcon from "../components/User";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color, size }) => (
+              <HomeIcon name="Home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color, size }) => (
+              <ProfileIcon name="Profile" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
